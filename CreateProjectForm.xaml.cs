1460229432e9610094c779aaaed75274cd93a46a
依力哈木江 阿布都喀迪尔 <yilihamujiang365@outlook.com>
2024-12-06 23:16:54 +0800

    添加项目文件。

---
+++ CreateProjectForm.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _537_FSCF
+{
+    /// <summary>
+    /// CreateProjectForm.xaml 的交互逻辑
+    /// </summary>
+    public partial class CreateProjectForm : Window
+    {
+        public string ProjectName { get; private set; }
+        public CreateProjectForm()
+        {
+            InitializeComponent();
+        }
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProjectName = ProjectNameTextBox.Text;
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
